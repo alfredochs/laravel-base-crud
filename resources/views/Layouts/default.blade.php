ea311733fd09b5d0a4fcd210e62e497c3fd855aa
alfredochs <chanta.alfredo@gmail.com>
2022-01-13 08:15:07 +0100

    secondary branch created

--- resources/views/Layouts/default.blade.php
+++ resources/views/Layouts/default.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Index View</title>
+    <title>@yield('title_page')</title>
     @include('partials.header_scripts')
 </head>
 
