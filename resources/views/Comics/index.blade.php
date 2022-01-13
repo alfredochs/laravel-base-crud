b2365710648296f1e468220ebd8c11f51e083496
alfredochs <chanta.alfredo@gmail.com>
2022-01-13 14:47:05 +0100

    edit & update done

--- resources/views/Comics/index.blade.php
+++ resources/views/Comics/index.blade.php
@@ -4,15 +4,14 @@
 
 @section('header_content')
     <h1 class="container">Header Content</h1>
-    @foreach ($comics as $comic)
-        <div>
-            <ul>
-                <li>
-                    <h5>{{ $comic->title }}</h5>
-                    <p>{{ $comic->description }}</p>
-                </li>
-            </ul>
-        </div>
-    @endforeach
+    <ul class="d-flex ">
+        @foreach ($comics as $comic)
+            <li class="mb-4 border p-2 list-unstyled">
+                <h5>{{ $comic->title }}</h5>
+                <p class="mb-0">{{ $comic->description }}</p>
+                <a href="{{ route('comics.edit', $comic->id) }}">Modifica</a>
+            </li>
+        @endforeach
+    </ul>
     <a href="{{ route('comics.create') }}">Link al form</a>
 @endsection
